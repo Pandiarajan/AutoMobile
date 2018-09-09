ff7b8bf0883e882892514098b2421a1b60cabdf7
Pandiarajan <pandiarajan.it@gmail.com>
2018-09-09 18:10:24 +0530

    Added support for HTTPS. No more Http access it will be redirected to Https site.

--- AutoApi/Startup.cs
+++ AutoApi/Startup.cs
@@ -46,6 +46,8 @@ namespace AutoApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseHsts();
+            app.UseHttpsRedirection();
             app.UseMiddleware<RequestLogMiddleware>();
             if (env.IsDevelopment())
             {
