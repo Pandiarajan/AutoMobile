348dd5d32d9b2e29b3a941d73b44da3d75dadc32
Pandiarajan <pandiarajan.it@gmail.com>
2018-09-09 12:50:30 +0530

    Refactor integration tests.

--- AutoApiIntegrationTest/CarsControllerTests.cs
+++ AutoApiIntegrationTest/CarsControllerTests.cs
@@ -15,12 +15,13 @@ namespace AutoApiIntegrationTest
         public CarsControllerTests()
         {
             _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("http://localhost:54696/api");
         }
 
         [Fact]
         public async void Get_Should_ReturnAllCars()
         {
-            var httpResult = await _httpClient.GetAsync("http://localhost:54696/api/odata/Cars");
+            var httpResult = await _httpClient.GetAsync("/odata/Cars");
             httpResult.EnsureSuccessStatusCode();
             var stringResult = await httpResult.Content.ReadAsStringAsync();
             var cars = JsonConvert.DeserializeObject<ODataResponse<Car>>(stringResult).Value;
@@ -30,7 +31,7 @@ namespace AutoApiIntegrationTest
         [Fact]
         public async void Get_Should_ReturnAllCars_OrderByPrice_FilterByPrice20000_Skip1_TakeTop5_WithCount()
         {
-            var httpResult = await _httpClient.GetAsync("http://localhost:54696/api/odata/Cars?$orderby=Price&$filter=Price le 20000&$skip=1&$top=5&$count=true");
+            var httpResult = await _httpClient.GetAsync("/odata/Cars?$orderby=Price&$filter=Price le 20000&$skip=1&$top=5&$count=true");
             httpResult.EnsureSuccessStatusCode();
             var stringResult = await httpResult.Content.ReadAsStringAsync();
             var carsResponse = JsonConvert.DeserializeObject<ODataResponse<Car>>(stringResult);
@@ -45,7 +46,7 @@ namespace AutoApiIntegrationTest
         public async void Get_Should_ReturnACarWhenPresent()
         {
             int carId = 1;
-            var httpResult = await _httpClient.GetAsync("http://localhost:54696/api/Cars/" + carId);
+            var httpResult = await _httpClient.GetAsync("/Cars/" + carId);
             httpResult.EnsureSuccessStatusCode();
             var stringResult = await httpResult.Content.ReadAsStringAsync();
             var car = JsonConvert.DeserializeObject<Car>(stringResult);
@@ -57,7 +58,7 @@ namespace AutoApiIntegrationTest
         {
             var json = JsonConvert.SerializeObject(new CarContract { Title = "My Car", FirstRegistration = new DateTime(2018, 02, 01), Fuel = Fuel.Diesel, IsNew = false, Mileage = 80, Price = 17000 });
             var stringContent = new StringContent(json, Encoding.UTF8, "application/json");
-            var httpResult = await _httpClient.PostAsync("http://localhost:54696/api/Cars/", stringContent);
+            var httpResult = await _httpClient.PostAsync("/Cars/", stringContent);
             httpResult.EnsureSuccessStatusCode();
             var stringResult = await httpResult.Content.ReadAsStringAsync();
             var car = JsonConvert.DeserializeObject<Car>(stringResult);
@@ -69,7 +70,7 @@ namespace AutoApiIntegrationTest
         {
             var json = JsonConvert.SerializeObject(new CarContract { Title = "My Car", FirstRegistration = new DateTime(2018, 02, 01), Fuel = Fuel.Diesel, IsNew = false, Mileage = 80, Price = 17000 });
             var stringContent = new StringContent(json, Encoding.UTF8, "application/json");
-            var httpResult = await _httpClient.PostAsync("http://localhost:54696/api/Cars/", stringContent);
+            var httpResult = await _httpClient.PostAsync("/Cars/", stringContent);
             var stringResult = await httpResult.Content.ReadAsStringAsync();
             var car = JsonConvert.DeserializeObject<Car>(stringResult);
 
@@ -79,10 +80,10 @@ namespace AutoApiIntegrationTest
             car.Price = newPrice;
             json = JsonConvert.SerializeObject(car);
             stringContent = new StringContent(json, Encoding.UTF8, "application/json");
-            httpResult = await _httpClient.PutAsync("http://localhost:54696/api/Cars/" + car.Id, stringContent);
+            httpResult = await _httpClient.PutAsync("/Cars/" + car.Id, stringContent);
             httpResult.EnsureSuccessStatusCode();
 
-            httpResult = await _httpClient.GetAsync("http://localhost:54696/api/Cars/" + car.Id);
+            httpResult = await _httpClient.GetAsync("/Cars/" + car.Id);
             stringResult = await httpResult.Content.ReadAsStringAsync();
             car = JsonConvert.DeserializeObject<Car>(stringResult);
             Assert.Equal(car.Title, newTitle);
@@ -94,7 +95,7 @@ namespace AutoApiIntegrationTest
         {
             var json = JsonConvert.SerializeObject(new Car { Id=1000, Title = "My Car", FirstRegistration = new DateTime(2018, 02, 01), Fuel = Fuel.Diesel, IsNew = false, Mileage = 80, Price = 17000 });
             var stringContent = new StringContent(json, Encoding.UTF8, "application/json");
-            var httpResult = await _httpClient.PutAsync("http://localhost:54696/api/Cars/" + 1000, stringContent);
+            var httpResult = await _httpClient.PutAsync("/Cars/" + 1000, stringContent);
 
             Assert.Equal(HttpStatusCode.NotFound, httpResult.StatusCode);
         }
@@ -103,7 +104,7 @@ namespace AutoApiIntegrationTest
         public async void Delete_Should_MarkAdvertisementDeleted()
         {
             int carId = 2;
-            var httpResult = await _httpClient.DeleteAsync("http://localhost:54696/api/Cars/" + carId);
+            var httpResult = await _httpClient.DeleteAsync("/Cars/" + carId);
             httpResult.EnsureSuccessStatusCode();
         }
     }
